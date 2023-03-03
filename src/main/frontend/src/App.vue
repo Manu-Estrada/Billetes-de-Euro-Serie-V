f2503c33782036a4c8dbff57abb408f083b94cc6
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-03-03 23:27:35 +0100

    first commit

---
+++ src/main/frontend/src/App.vue
@@ -0,0 +1,5 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
---
+++ src/main/java/com/manu/billetes_zona_euro_serie_v/BilletesZonaEuroSerieVApplication.java
@@ -0,0 +1,13 @@
+package com.manu.billetes_zona_euro_serie_v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilletesZonaEuroSerieVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilletesZonaEuroSerieVApplication.class, args);
+	}
+
+}
