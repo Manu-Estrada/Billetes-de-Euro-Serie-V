041fda017f899924fbae36ecbf198b9c05652e9f
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-03-05 14:05:26 +0100

    first commit

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -3,3 +3,26 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<template>
+    <div>
+      <Header
+        :image-src="'ruta/de/la/imagen.png'"
+        :image-alt="'Descripción de la imagen'"
+        :header-text="'Mi encabezado con imagen y texto'"
+      />
+      <main>
+        <!-- Aquí va el contenido principal de la página -->
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  import Header from '@/components/Header.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+  </script>
---
+++ src/main/frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+      <img :src="src/main/frontend/src/assets/img/Captura de pantalla (176)logo 1.png" :alt="imageAlt">
+      <h1>{{ headerText }}</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header',
+    props: {
+      imageSrc: String,
+      imageAlt: String,
+      headerText: String
+    }
+  }
+  </script>
